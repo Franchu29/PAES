64289ef4cfc83a4f2f894d122d1e214aa5c188e1
Franchu <102395117+Franchu29@users.noreply.github.com>
2026-08-03 17:22:24 -0400

    Delete src directory

--- src/config/ollama.js
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-
-const ollama = axios.create({
-  baseURL: process.env.OLLAMA_URL,
-});
-
-export default ollama;
--- src/services/seed.js
+++
@@ -1,9 +0,0 @@
-import { addToVectorDB } from "./vector.service.js";
-
-const run = async () => {
-  await addToVectorDB("El teorema de Pitágoras establece que a² + b² = c².");
-  await addToVectorDB("La fotosíntesis es el proceso por el cual las plantas producen energía.");
-  await addToVectorDB("La célula es la unidad básica de los seres vivos.");
-};
-
-run();
